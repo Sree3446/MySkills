8495d4449e9080ef1ae87f1941ff8d6304f62c1c
Sree3446 <123738826+Sree3446@users.noreply.github.com>
2023-03-10 13:27:40 +0530

    Add files via upload

---
+++ Assihnment2.java
@@ -0,0 +1,20 @@
+
+public class Assihnment2 {
+	
+	public static void main(String[]args) {
+		
+		int n = 5;
+		
+		int a = 1;
+		
+		for(int i = 1; i<n; i++) {
+			
+			for(int j = 1;j<n;j++) {
+				
+				System.out.print(i+ " ");
+			}
+			System.out.println();
+		}
+	}
+
+}
